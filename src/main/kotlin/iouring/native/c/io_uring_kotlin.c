831148c3768ac84882c673c0035034be9ed8b1a8
frevib <hdevries@fastmail.com>
2020-03-07 14:18:47 +0100

    static C functions.

--- src/main/kotlin/iouring/native/c/io_uring_kotlin.c
+++ src/main/kotlin/iouring/native/c/io_uring_kotlin.c
@@ -5,7 +5,7 @@
 //#include "../headers/io_uring_kotlin.h"
 
 
-JNIEXPORT jint JNICALL Java_iouring_IOuringNativeBindings_ioUringQueueInit (JNIEnv * env, jobject obj, jint ring_size)
+static jint JNICALL Java_iouring_IOuringNativeBindings_ioUringQueueInit (JNIEnv * env, jobject obj, jint ring_size)
 {
     struct io_uring ring;
     int result = io_uring_queue_init(ring_size, &ring, 0);
@@ -19,7 +19,7 @@ JNIEXPORT jint JNICALL Java_iouring_IOuringNativeBindings_ioUringQueueInit (JNIE
 };
 
 
-jint JNICALL Java_iouring_IOuringNativeBindings_ioUringQueueInitParams (JNIEnv * env, jobject obj, jint ring_size)
+static jint JNICALL Java_iouring_IOuringNativeBindings_ioUringQueueInitParams (JNIEnv * env, jobject obj, jint ring_size)
 {
     struct io_uring ring;
     struct io_uring_params params;
@@ -35,7 +35,7 @@ jint JNICALL Java_iouring_IOuringNativeBindings_ioUringQueueInitParams (JNIEnv *
     return result;
 }
 
-jint JNICALL Java_iouring_IOuringNativeBindings_ioUringQueueInitParams_1IORING_1FEAT_1FAST_1POLL (JNIEnv * env, jobject obj, jint ring_size)
+static jint JNICALL Java_iouring_IOuringNativeBindings_ioUringQueueInitParams_1IORING_1FEAT_1FAST_1POLL (JNIEnv * env, jobject obj, jint ring_size)
 {
     struct io_uring ring;
     struct io_uring_params params;
